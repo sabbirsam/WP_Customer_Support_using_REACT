24394eb4bd0b2a65e1f0f18833510b226178d003
sabbirsam <samiubat14@gmail.com>
2022-11-01 19:29:37 +0600

    complete staff page

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '902f022cc8730bfce169');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '8d59dc86c8b8afe517f0');
--- src/components/Data-Table-Staff/DataTableStaff.jsx
+++ src/components/Data-Table-Staff/DataTableStaff.jsx
@@ -2,45 +2,138 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import './data-table.staff.scss'
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    field: "name",
+    headerName: "Name",
+    width: 280,
+    renderCell: (params) => {
+      return (
+        <div className="wcs_cellWithImg">
+          <img className="wcs_cellImg" src={params.row.img} alt="avatar" />
+          {params.row.username}
+        </div>
+      );
+    },
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
+    field: "email",
+    headerName: "Email",
+    width: 300,
+  },
+
+  {
+    field: "contact",
+    headerName: "Contact",
+    width: 200,
+  },
+  {
+    field: "status",
+    headerName: "Status",
     width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    renderCell: (params) => {
+      return (
+        <div className={`cellWithStatus ${params.row.status}`}>
+          {params.row.status}
+        </div>
+      );
+    },
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Snow",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    status: "inactive",
+    email: "wppool_snow@gmail.com",
+    contact: 3684845,
+  },
+  {
+    id: 2,
+    username: "Jamie Lannister",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_2snow@gmail.com",
+    status: "active",
+    contact: 4684842,
+  },
+  {
+    id: 3,
+    username: "Lannister",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_3snow@gmail.com",
+    status: "active",
+    contact: 4684845,
+  },
+  {
+    id: 4,
+    username: "Stark",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_4snow@gmail.com",
+    status: "active",
+    contact: 1684846,
+  },
+  {
+    id: 5,
+    username: "Targaryen",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_5snow@gmail.com",
+    status: "active",
+    contact: 2684842,
+  },
+  {
+    id: 6,
+    username: "Melisandre",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_6snow@gmail.com",
+    status: "inactive",
+    contact: 1684845,
+  },
+  {
+    id: 7,
+    username: "Clifford",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_7snow@gmail.com",
+    status: "active",
+    contact: 4684844,
+  },
+  {
+    id: 8,
+    username: "Frances",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_8snow@gmail.com",
+    status: "active",
+    contact: 3668484,
+  },
+  {
+    id: 9,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_9snow@gmail.com",
+    status: "active",
+    contact: 6568484,
+  },
+  {
+    id: 10,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "wppool_21snow@gmail.com",
+    status: "inactive",
+    contact: 6568484,
+  },
 ];
+
+
 
 const DataTableStaff = () => {
   return (
     <div className='wcs_datatable_staff'>
       <DataGrid
-        rows={rows}
-        columns={columns}
+        rows={userRows}
+        columns={userColumns}
         pageSize={8}
         rowsPerPageOptions={[8]}
         checkboxSelection
