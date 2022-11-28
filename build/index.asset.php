f03fa1a1728da1bbba3f8e40efc18aaf40816cb3
sabbirsam <samiubat14@gmail.com>
2022-11-28 17:55:11 +0600

    fix status design

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '2fbea01a450b29b00fc1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'c711807c931211ca4007');
